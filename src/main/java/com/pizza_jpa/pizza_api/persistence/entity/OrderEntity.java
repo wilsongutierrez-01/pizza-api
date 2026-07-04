25345f123b57c9159fbd842cb14d44626a13e650
Wilson Gutierrez <adolfoguti001@outlook.com>
2026-07-03 23:01:23 -0600

    Add relation to tables

--- src/main/java/com/pizza_jpa/pizza_api/persistence/entity/OrderEntity.java
+++ src/main/java/com/pizza_jpa/pizza_api/persistence/entity/OrderEntity.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "pizza_order")
@@ -32,4 +33,11 @@ public class OrderEntity {
 
     @Column(name = "additional_notes", length = 200)
     private String additionalNotes;
+
+    @OneToOne
+    @JoinColumn(name = "id_customer", referencedColumnName = "id_customer", insertable = false, updatable = false)
+    private CustomerEntity customer;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItemEntity> items;
 }
--- src/main/java/com/pizza_jpa/pizza_api/persistence/entity/OrderItemEntity.java
+++ src/main/java/com/pizza_jpa/pizza_api/persistence/entity/OrderItemEntity.java
@@ -28,4 +28,13 @@ public class OrderItemEntity {
 
     @Column(nullable = false, columnDefinition = "Decimal(2,1)")
     private Double price;
+
+    @OneToOne
+    @JoinColumn(name = "id_pizza", referencedColumnName = "id_pizza", insertable = false, updatable = false)
+    private PizzaEntity pizza;
+
+    @ManyToOne
+    @JoinColumn(name = "id_order", referencedColumnName = "id_order", insertable = false, updatable = false)
+    private OrderEntity order;
+
 }
